2dbbf6f51a8adba1c5cc46c5f1029fc2780b4b8e
Jamil Muradov <126609769+JamilDeveloper1@users.noreply.github.com>
2023-06-28 01:25:17 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null;
-const API_KEY = "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";
+const API_KEY = "YOUR API KEY";
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
